06ba5ff84217b3e12bc5f99076a99cb4997ffd08
SOUFIANE IDRISSI <idrissisoufiane2000@gmail.com>
2024-01-19 23:04:03 +0100

    swiper

--- src/Components/3C-New/3~1C-Movies/3~1C-MoviesSwiper/MoviesSlide.jsx
+++ src/Components/3C-New/3~1C-Movies/3~1C-MoviesSwiper/MoviesSlide.jsx
@@ -1,6 +1,6 @@
 // import "./MoviesSlide.css";
 
-// import { useState } from "react";
+// import { useEffect, useState } from "react";
 
 // // Import Swiper styles
 // import "swiper/css";
@@ -20,6 +20,14 @@
 //   const [movies, setMovies] = useState([]);
 
 //   const [activeSlide, setActiveSlide] = useState(null);
+
+//   useEffect(() => {
+//     console.log(activeSlide);
+//   }, [activeSlide]);
+
+//   // const [flag, setFlag] = useState(false);
+
+//   const [popUpBox, setPopUpBox] = useState(false);
 
 //   // === FETCH DATA ===
 //   const api_key = "dfdb8fba093651a08fc61b28b9c3c796";
@@ -41,7 +49,7 @@
 //   // === FETCH DATA ===
 
 //   return (
-//     <div className="container ddd">
+//     <div className="parent-container">
 //       <Swiper
 //         dir="rtl"
 //         className="swiper"
@@ -82,7 +90,11 @@
 //             <SwiperSlide
 //               className={`swiperSlide `}
 //               key={movie.id}
-//               onClick={() => setActiveSlide(movie.id)}
+//               onClick={() => {
+//                 setActiveSlide(movie.id);
+//                 // setFlag(true);
+//                 // setPopUpBox(true);
+//               }}
 //             >
 //               <img
 //                 src={`${base_img}${movie.poster_path}`}
@@ -94,31 +106,38 @@
 //                 <p>Play Trailer</p>
 //               </div>
 
-//               <div
+//               {/* <div
 //                 className={`poup ${
-//                   activeSlide === movie.id ? "active" : undefined
+//                   activeSlide == movie.id ? "active" : undefined
 //                 }`}
 //               >
 //                 hello
-//               </div>
+//               </div> */}
+
+//               {activeSlide == movie.id ? setPopUpBox(true) : setPopUpBox(false)}
 //             </SwiperSlide>
 //           ))}
 //       </Swiper>
+//       {/* <div className={`overlay ${flag == true ? "active" : undefined}`}></div> */}
+//       <div className={`popUpBox ${popUpBox == true ? "active" : undefined}`}>
+//         Hello
+//       </div>
 //     </div>
 //   );
 // };
 
 // export default MoviesSlide;
 
-// // movies &&
-// //           movies.length > 0 &&
-// //           movies.map((mv)
+// {
+//   // <div className={`overlay ${flag == true ? "active" : undefined}`}></div>
+//   // /* <div className={`container ${flag == true ? "active" : undefined}`}></div> */
+// }
 
-// ====================================================================================================
+// ==============================================================================================================
 
 import "./MoviesSlide.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Import Swiper styles
 import "swiper/css";
@@ -133,16 +152,13 @@ import "swiper/css/scrollbar";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 const MoviesSlide = () => {
-  // const [isHovered, setIsHovered] = useState(false);
-
   const [movies, setMovies] = useState([]);
 
   const [activeSlide, setActiveSlide] = useState(null);
 
-  // === FETCH DATA ===
-  const api_key = "dfdb8fba093651a08fc61b28b9c3c796";
-  // const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmRiOGZiYTA5MzY1MWEwOGZjNjFiMjhiOWMzYzc5NiIsInN1YiI6IjY1YTNhODMzZDM1ZGVhMDEzMTQzMDczNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vM6qeeLj6z6mF4j6nPFTcuqwxcH99UZ99SvIBXfnYVM";
+  const [popUpBox, setPopUpBox] = useState(false);
 
+  const api_key = "dfdb8fba093651a08fc61b28b9c3c796";
   const base_url = "https://api.themoviedb.org/3";
   const get_movies = "/discover/movie";
   const base_img = "https://image.tmdb.org/t/p/w500";
@@ -152,11 +168,18 @@ const MoviesSlide = () => {
     const data = await response.json();
     setMovies(data.results);
   }
+
   const api_url = `${base_url}/${get_movies}?api_key=${api_key}`;
 
   fetchMovies(api_url);
 
-  // === FETCH DATA ===
+  useEffect(() => {
+    if (activeSlide) {
+      setPopUpBox(true);
+    } else {
+      setPopUpBox(false);
+    }
+  }, [activeSlide]);
 
   return (
     <div className="parent-container">
@@ -200,7 +223,9 @@ const MoviesSlide = () => {
             <SwiperSlide
               className={`swiperSlide `}
               key={movie.id}
-              onClick={() => setActiveSlide(movie.id)}
+              onClick={() => {
+                setActiveSlide(movie.id);
+              }}
             >
               <img
                 src={`${base_img}${movie.poster_path}`}
@@ -212,17 +237,13 @@ const MoviesSlide = () => {
                 <p>Play Trailer</p>
               </div>
 
-              <div
-                className={`poup ${
-                  activeSlide === movie.id ? "active" : undefined
-                }`}
-              >
-                hello
-              </div>
+              {activeSlide == movie.id && <div>Hello</div>}
             </SwiperSlide>
           ))}
       </Swiper>
-      <div className={`container ddd `}>{/* Your content here */}</div>
+      <div className={`popUpBox ${popUpBox == true ? "active" : undefined}`}>
+        Hello
+      </div>
     </div>
   );
 };
